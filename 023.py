53b54f5a86ebd12d7903904d760db54d647529f9
RynhAleh <workinplus@yandex.ru>
2022-08-29 20:55:15 +0300

    matrix diagonal maximum

--- 023.py
+++ 023.py
@@ -5,13 +5,15 @@ spis = []
 sub_spis = []
 razm = elem = mx_elem = 0
 
-# Запрос размерности матрицы с обработной исключений
+# Запрос размерности матрицы с обработкой исключений
 try:
     razm = int(input("Размерность матрицы (2-20): "))
 except ValueError:
     print("Ввёденное значение не являются целым числом!")
+    exit(0)
 if not (0 < razm < 21):
     print("Ввёденное значение должно быть в диапазоне (2-20)!")
+    exit(0)
 
 # Заполнение матрицы razm х razm случайными значениями (от 0 до 200)
 print(f'Cгенерирована матрица {razm} х {razm} со значениями элементов от 0 до 200:\n')
@@ -19,7 +21,7 @@ for i in range(0, razm):
     for j in range(0, razm):
         elem = random.randint(0, 200)
         sub_spis.append(elem)
-        print(elem, end='\t')
+        print(str(elem).rjust(4), end='')
     print()
     spis.append(sub_spis)
     sub_spis = []
