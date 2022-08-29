d87e83d8ae32667c352094a0f19928b1013f1667
RynhAleh <workinplus@yandex.ru>
2022-08-29 10:07:52 +0300

    Modules are done

---
+++ 007.py
@@ -0,0 +1,18 @@
+a = input("Введите возраст (1-99): ")
+try:
+    a = int(a)
+except Exception as ex:
+    print("Введенное значение не является числом!")
+else:
+    if a < 1 or a > 99:
+        print("Введенный вами возраст выходит за рамки 1-99!")
+    else:
+        if a > 10 and a < 15:
+            years = "лет"
+        elif str(a)[-1:] in ["2", "3", "4"]:
+            years = "года"
+        elif str(a)[-1:] == "1":
+            years = "год"
+        else:
+            years = "лет"
+        print(f'Мне {a} {years}.')
