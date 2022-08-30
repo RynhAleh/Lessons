945f427de983368864e6f9d6279d5729251f91c0
RynhAleh <workinplus@yandex.ru>
2022-08-30 13:21:59 +0300

    in both sets

---
+++ 026.py
@@ -0,0 +1,3 @@
+set1 = {'Microsoft', 'Oracle', 'Apple', 'Linux', 'Yandex', 'Atlassian', 'Dropbox', 'Zoom', 'SAP', 'NetApp', 'Salesforce'}
+set2 = {'Facebook', 'Google', 'Apple', 'Alphabet', 'Adobe', 'Salesforce', 'Atlassian', 'Slack', 'SAP', 'NetApp'}
+print(set1 & set2)
