42a1a9a8a0377e458b228e7ee9b6a63ffa187ca3
RynhAleh <workinplus@yandex.ru>
2022-09-04 09:07:06 +0300

    Fibo

--- 016.py
+++ 016.py
@@ -3,17 +3,15 @@
 
 
 try:
-    a = int(input("До какого элемента вывести ряд Фибоначчи: "))
+    r = int(input("До какого элемента вывести ряд Фибоначчи: "))
 except ValueError:
     print("Ввёденное значение не является натуральным числом!")
 else:
-    if a <= 0:
+    if r <= 0:
         print("Ввёденное значение меньше нуля!")
     else:
-        tmp = ""
-        fibo = [1, 1, 2]
-        while a > 0:
-            a -= 1
-            fibo = [fibo[1], fibo[2], fibo[1]+fibo[2]]
-            tmp = f'{tmp} {fibo[0]}'
-        print(tmp)
+        i, a, b = 0, 0, 1
+        while i < r:
+            print(a, end=' ')
+            i, a, b = i + 1, b, a + b
+
