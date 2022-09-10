5f6f37e38e5ce6ac39ed09a9017d03c15665bf3c
RynhAleh <workinplus@yandex.ru>
2022-09-10 23:31:10 +0300

    samples of encapsulation

---
+++ 046.py
@@ -0,0 +1,31 @@
+# В классе из предыдущего урока приведите пример инкапсуляции.
+
+class Animal:
+    def about1(self) -> str:                  # к __about1() имеют доступ как объекты родителя Animal, так и объекты потомка Dog
+        return "Это - " + self.__info()       # __info() используется только внутри класса (ИНКАПСУЛЯЦИЯ)
+
+    @staticmethod
+    def __info() -> str:                      # __info() не может вызываться извне (ИНКАПСУЛЯЦИЯ)
+        return "живое существо"
+
+
+class Dog(Animal):
+    def about2(self) -> str:                  # к __about2() имеют доступ объекты потомка Dog
+        return "Собака " + self.__whatdoes()  # __whatdoes() используется только внутри класса (ИНКАПСУЛЯЦИЯ)
+
+    @staticmethod
+    def __whatdoes() -> str:
+        return "лает"
+
+
+st1 = Animal()
+st2 = Dog()
+print(f'Animal: {st1.about1()}')              # вызов через экземпляр Animal обычного метода класса Animal
+print()
+print(f'Dog: {st2.about1()}')                 # вызов через экземпляр Dog обычного метода класса-предка Animal
+print(f'Dog: {st2.about2()}')                 # вызов через экземпляр Dog обычного метода класса Dog
+print(f'Animal: {st1.__info()}')              # попытка вызова через экземпляр Animal приватного метода класса Animal
+"""
+AttributeError: 'Animal' object has no attribute '__info'
+Область видимости методов __info() и __whatdoes() - только внутри соответствующих классов. 
+"""
