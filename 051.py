d7b2d1b646afbc8e7cdd5f29ff1e5254b3455a25
RynhAleh <workinplus@yandex.ru>
2022-09-16 15:24:20 +0300

    select from database

---
+++ 051.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+conn = sqlite3.connect("mydatabase.db")
+curs = conn.cursor()
+
+print("-----------ВСЕ ПЕРСОНАЛИИ:-------------")
+curs.execute("SELECT * FROM PERSONS")
+print(*curs.fetchall(), sep="\n")
+
+print("\n-----------КОНФЕРЕНЦИИ С ИЮЛЯ ПО АВГУСТ 2022:-------------")
+curs.execute("SELECT * FROM CONFERENCES WHERE date_conf BETWEEN '2022-07-01' AND '2022-08-31'")
+print(*curs.fetchall(), sep="\n")
+
+print("\n-----------ФИО ПРИГЛАШЕННЫХ НА ВЫШЕУКАЗАННЫЕ КОНФЕРЕНЦИИ (В АЛФАВИТНОМ ПОРЯДКЕ):-------------")
+curs.execute("SELECT full_name FROM PERSONS WHERE ID IN("
+             "SELECT ID_person FROM PARTICIPANTS WHERE ID_conference IN("
+             "SELECT ID FROM CONFERENCES WHERE date_conf BETWEEN '2022-07-01' AND '2022-08-31'"
+             ")) ORDER BY full_name")
+print(*curs.fetchall(), sep="\n")
+
+
+curs.close()
+conn.close()
